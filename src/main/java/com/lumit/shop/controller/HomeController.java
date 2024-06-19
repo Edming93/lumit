0c0bbc8414140c79fae513d81cf87efa6c392cc1
Choi-Shin <shincho1990@naver.com>
2024-06-19 19:20:06 +0900

    세팅

--- src/main/java/com/lumit/shop/controller/HomeController.java
+++ src/main/java/com/lumit/shop/controller/HomeController.java
@@ -2,12 +2,19 @@ package com.lumit.shop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/th")
+    public String th_home() {
+        return "th/index";
     }
 }
