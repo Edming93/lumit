4bb8dc251a771d921f3861c373f5284f5d534c9f
Edming93 <gon695@naver.com>
2024-06-19 20:03:39 +0900

    스프링 기본 세팅 수정

--- src/main/java/com/lumit/shop/controller/HomeController.java
+++ src/main/java/com/lumit/shop/controller/HomeController.java
@@ -2,19 +2,11 @@ package com.lumit.shop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
         return "index";
-    }
-
-    @GetMapping("/th")
-    public String th_home() {
-        return "th/index";
     }
 }
