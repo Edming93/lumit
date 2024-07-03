07af1a8f530cce0fdb27cba11c8d5cd622fa9847
Edming93 <gon695@naver.com>
2024-07-03 19:48:25 +0900

    mybatis 세팅 수정

--- src/main/java/com/lumit/shop/ShopApplication.java
+++ src/main/java/com/lumit/shop/ShopApplication.java
@@ -1,10 +1,7 @@
 package com.lumit.shop;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@MapperScan("com.lumit.shop.admin.repository")
 @SpringBootApplication
 public class ShopApplication {
 
