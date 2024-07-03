0b770d3f49bedba27c74d36b4f17a2e87b0357cc
Edming93 <gon695@naver.com>
2024-07-03 21:15:11 +0900

    신규 : 시큐리티 기본 세팅

--- src/main/java/com/lumit/shop/admin/controller/UserController.java
+++ src/main/java/com/lumit/shop/admin/controller/UserController.java
@@ -4,6 +4,10 @@ import com.lumit.shop.admin.model.user;
 import com.lumit.shop.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +27,21 @@ public class UserController {
     @GetMapping("/user")
     public List<user> user() {
         return userService.getUserList();
+    }
+
+    @GetMapping("/user/detail")
+    public ResponseEntity user(@AuthenticationPrincipal UserDetails userDetails) {
+        return new ResponseEntity<>(userDetails.getUsername() + " / " +userDetails.getPassword(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api1")
+    public ResponseEntity api1() {
+        return new ResponseEntity<>("api1 입니다.", HttpStatus.OK);
+    }
+
+    @GetMapping("/api2")
+    public ResponseEntity api2() {
+        return new ResponseEntity<>("api2 입니다.", HttpStatus.OK);
     }
 
 }
