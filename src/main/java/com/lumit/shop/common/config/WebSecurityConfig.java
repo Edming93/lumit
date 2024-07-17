91d4ba39802d8f4e2d2b621a67b07a5be92a3dce
Choi-Shin <shincho1990@naver.com>
2024-07-17 20:45:00 +0900

    로그아웃

--- src/main/java/com/lumit/shop/common/config/WebSecurityConfig.java
+++ src/main/java/com/lumit/shop/common/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.lumit.shop.common.config;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -24,6 +26,7 @@ public class WebSecurityConfig {
 
         http.authorizeHttpRequests((authorizeRequests) ->
                 authorizeRequests.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/admin").authenticated()
                         .requestMatchers("/", "/login").permitAll()
                         .requestMatchers("/api1").hasRole("user")
                         .requestMatchers("/api2").hasRole("admin")
@@ -35,13 +38,15 @@ public class WebSecurityConfig {
                         .passwordParameter("password")
                         .defaultSuccessUrl("/", true)
         );
+        http.logout((logout) -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).deleteCookies("JSESSIONID").logoutSuccessUrl("/"));
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user1@lumit.com").password("1234").roles("user").build());
+
+        manager.createUser(User.withUsername("user1").password("1234").roles("user").build());
         return manager;
     }
 
