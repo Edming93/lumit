2d1f90a4ae50f120b511f13b13e5ba7115ec4461
Edming93 <gon695@naver.com>
2024-07-31 20:50:54 +0900

    ...

--- src/main/java/com/lumit/shop/common/model/User.java
+++ src/main/java/com/lumit/shop/common/model/User.java
@@ -5,15 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     private String userId;
     private String userName;
     private String roleId;
@@ -26,5 +31,49 @@ public class User {
     private String regId;
     private Timestamp modDt;
     private String modId;
+
+    private List<TbMenu> authorities = new ArrayList<>();
+
+    public void setAuthorities (List<TbMenu> authorities) {
+        this.authorities = authorities;
+    }
+
+    public void addAuthority (TbMenu menu) {
+        authorities.add(menu);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.<GrantedAuthority> unmodifiableCollection(authorities);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    public String getUserName() {
+        return userId;
+    }
 
 }
