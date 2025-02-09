936df622f21a4ce0b1aa492a4a1a44b179462278
Choi-Shin <shincho1990@naver.com>
2025-02-09 16:06:07 +0900

    aa

--- src/main/java/com/lumit/shop/common/security/WebSecurityConfig.java
+++ src/main/java/com/lumit/shop/common/security/WebSecurityConfig.java
@@ -24,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class WebSecurityConfig {
     private final OAuth2UserService oAuth2UserService;
     private static final String[] WHITE_LIST = {
-            "/favicon**", "/", "/main", "/main/member/createUser", "/main/member/findUser", "/lumit/**", "/error/**", "/api/**", "/auth/**", "/common/**", "/rest/**"
+            "/favicon**", "/", "/main", "/main/member/createUser", "/main/member/findUser", "/lumit/**", "/error/**", "/api/**", "/auth/**", "/common/**", "/rest/**", "/lumitFiles/**"
 
     };
     private final CustomAuthorizationManager customAuthorizationManager;
