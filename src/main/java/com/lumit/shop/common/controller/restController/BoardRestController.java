6e3b5fcd719a60267ef9ee01a67e312b1d2be3ba
edming <gon695@naver.com>
2025-03-04 21:35:03 +0900

    jdk못읽는 것 수정, 게시판 -parameters 에러 (BoardRestController 분기시 오류) 해결, 상품관리 리스트 항목추가
    BoardRestController분기시에 어노테이션 추가하지 않아서 생긴 오류,,,

--- src/main/java/com/lumit/shop/common/controller/restController/BoardRestController.java
+++ src/main/java/com/lumit/shop/common/controller/restController/BoardRestController.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ public class BoardRestController {
     private final BoardService boardService;
 
     @GetMapping(value = "/boards")
-    public @ResponseBody ResponseEntity<?> boardList(String menuCd, SearchDto search, TbBoard board, @PageableDefault(size = 10) Pageable pageable) throws IOException {
+    public @ResponseBody ResponseEntity<?> boardList(@RequestParam("menuCd") String menuCd, @ModelAttribute SearchDto search, @ModelAttribute TbBoard board, @PageableDefault(size = 10) Pageable pageable) throws IOException {
         board.setMenuCd(menuCd);
         if (search.getTitle() != null) {
             board.setTitle(search.getTitle());
